041c387ba7b5ca5dda84f73a3b1129a6b2e572c4
andrubuntu <andrubuntu@016ccdcc-ba80-11de-81bc-59952d0011f9>
2013-07-04 17:43:42 +0000

    added description for fortax_compare

--- trunk/fortax/fortax_compare.f90
+++ trunk/fortax/fortax_compare.f90
@@ -1,3 +1,28 @@
+
+! This file is part of the FORTAX library;
+
+! FORTAX is free software: you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation, either version 3 of the License, or
+! (at your option) any later version.
+! 
+! FORTAX is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+! 
+! You should have received a copy of the GNU General Public License
+! along with FORTAX.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
+
+! fortax_compare
+! -----------------------------------------------------------------------
+! module provides the ability to create a database of family types and
+! incomes which can then easily be compated against. AS 04/07/13
+
+
 module fortax_compare
 
     use fortax_realtype
